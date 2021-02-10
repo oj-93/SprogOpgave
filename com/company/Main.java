743d08e5df83e7725713cd34c21ed812876c0044
jonasmik <jonassoldath@live.dk>
2021-02-10 12:38:54 +0100

    Switch statement

--- com/company/Main.java
+++ com/company/Main.java
@@ -6,21 +6,39 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
         Dialog dialog = new English();
+       
 
         Sprog sprog = new Sprog();
+        String quit = null;
+        String a;
+        while (quit != "Quit") {
 
-        while (!getString(dialog.quit()).equalsIgnoreCase("q")) {
+            switch (a = getString("dit valg")) {
 
+            case "Capital":
+                System.out.println("The capital is " + dialog.getCapital());
+                break;
 
-            if (getString("press y to change language").equalsIgnoreCase("y")) {
+            case "Population":
+                System.out.println("The population of the country is " + dialog.getPopulation());
+                break;
 
+            case "Change":
                 dialog = sprog.SkiftSprog(getString("da / eng / svk / fnsk / por"));
-            }
-            System.out.println(dialog.selectedLanguage() + " " + dialog.getLanguage());
-            System.out.println("The capital is " + dialog.getCapital());
-            System.out.println("The population of the country is " + dialog.getPopulation());
+                break;
+
+            case "Current":
+                System.out.println(dialog.selectedLanguage() + " " + dialog.getLanguage());
+                break;
+
+            case "Quit":
+                quit = "Quit";
+                break;
+        }
+
+
+
         }
 
 
